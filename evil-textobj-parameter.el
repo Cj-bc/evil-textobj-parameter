c4f62be46b062289b27ad55fedbe3a71e048c37d
Cj-bc <cj.bc-sd@outlook.jp>
2021-11-15 11:54:51 +0900

    Add: LICENSE code under MPL2

--- evil-textobj-parameter.el
+++ evil-textobj-parameter.el
@@ -11,6 +11,10 @@
 
 ;; This file is not part of GNU Emacs.
 
+;; This Source Code Form is subject to the terms of the Mozilla Public
+;; License, v. 2.0. If a copy of the MPL was not distributed with this
+;; file, You can obtain one at https://mozilla.org/MPL/2.0/.
+
 ;;; Commentary:
 ;; This package is port of https://github.com/sgur/vim-textobj-parameter
 ;; 
